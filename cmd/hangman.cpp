21a39033d4d3908d749f1e446c0a713858b01f40
Brian Lee <leebr1>
2017-05-18 15:35:04 -0400

    addded a

---
+++ cmd/hangman.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int gen(){ //will generate the the randomized word, out of an array instantiated in main
+
+}
+int main(){
+    //to do with while loop?
+    //recursion?
+}
