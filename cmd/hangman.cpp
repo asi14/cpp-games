4b7689eaad1e2b2a8bb6acc2bac5e8ab84fad3cb
Brian Lee <blee025432@gmail.com>
2017-05-18 20:17:04 -0400

    about to parse output

--- cmd/hangman.cpp
+++ cmd/hangman.cpp
@@ -12,14 +12,40 @@ int gen(){
 	return select;
 }
 
-int process(){
+string parseOutput(std::string random, std::string output, std::string test){
 	std::cout << "hello world" << endl;
+	return output;	
+}
+
+int process(std::string random){
+	string output = "";
+	for(int i = 0; i < random.length(); i++){ //replace later with something like "*' * random.length() or something
+		output+="*";	
+	}
+	int i = 10;
+	bool test = false;
+	while(i > 0 || test == false){
+		std::string test;
+		std::cout << "enter a character" << endl;
+		std::getline(std::cin, test);
+		if(random.find(test)==std::string::npos){
+			std::cout << "the char is not there\n" << "You have " << i-1 << " chances left\n" << "here is your current hint\n" << output << endl;
+			i--;	
+		}
+		else{
+			std::cout << test << "was found\n" << "here is your current hint\n" << endl;
+			output = parseOutput(random, output, test);
+			std::cout << output << endl;			
+		}
+	}
+	std::cout << output << endl;
+	std::cout << random << endl;
 	return 0;	
 }
+
 int main(){
 	string arr[] = {"hello","world","bye","michele","ubuntu","vim","master","race","trains","like"};
 	std::string rando = arr[gen()];
-	std::cout << rando << endl;
-	process();
+	process(rando);
 	return 0;
 }
