59f7027c5f7450bfccb8d4627a771b6ad172cc50
Brian Lee <blee025432@gmail.com>
2017-05-20 16:16:54 -0400

    added validate

--- tictactoe/cmd/game.cpp
+++ tictactoe/cmd/game.cpp
@@ -2,6 +2,35 @@
 #include <string>
 
 using namespace std;
+
+int input(int (*rep)[3][3], int i, int a, int input){
+	(*rep)[i][a]=input;
+	return 0;
+}
+//0 for no winner, 1 for cpu, 2 for user. requires later testing for ties
+int  validate(int (*rep)[3][3]){
+	int output = 0;
+	if((*rep)[0][0]==(*rep)[0][1]==(*rep)[0][2]==1){output = 1;}
+	if((*rep)[0][0]==(*rep)[0][1]==(*rep)[0][2]==2){output = 2;}
+	if((*rep)[1][0]==(*rep)[1][1]==(*rep)[1][2]==1){output = 1;}
+	if((*rep)[1][0]==(*rep)[1][1]==(*rep)[1][2]==2){output = 2;}
+	if((*rep)[2][0]==(*rep)[2][1]==(*rep)[2][2]==1){output = 1;}
+	if((*rep)[2][0]==(*rep)[2][1]==(*rep)[2][2]==2){output = 2;}
+
+	if((*rep)[0][0]==(*rep)[1][0]==(*rep)[2][0]==1){output = 1;}
+	if((*rep)[0][0]==(*rep)[1][0]==(*rep)[2][0]==2){output = 2;}
+	if((*rep)[0][1]==(*rep)[1][1]==(*rep)[2][1]==1){output = 1;}
+	if((*rep)[0][1]==(*rep)[1][1]==(*rep)[2][1]==2){output = 2;}
+	if((*rep)[0][2]==(*rep)[1][2]==(*rep)[2][2]==1){output = 1;}
+	if((*rep)[0][2]==(*rep)[1][2]==(*rep)[2][2]==2){output = 2;}
+
+	if((*rep)[0][0]==(*rep)[1][1]==(*rep)[2][2]==1){output=1;}
+	if((*rep)[0][0]==(*rep)[1][1]==(*rep)[2][2]==2){output=2;}
+	if((*rep)[0][2]==(*rep)[1][1]==(*rep)[2][0]==1){output=1;}
+	if((*rep)[0][2]==(*rep)[1][1]==(*rep)[2][0]==2){output=2;}
+
+	return output;
+}
 int printBoard(int (*rep)[3][3]){
 	for(int i = 0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
@@ -9,6 +38,7 @@ int printBoard(int (*rep)[3][3]){
 		}
 		cout << endl;
 	}
+	return 0;
 }
 
 int main(){
