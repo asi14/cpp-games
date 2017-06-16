0dd2684d9aca38d1ffeedf080555a3a69cfdbe7b
Brian Lee <blee025432@gmail.com>
2017-06-16 11:59:18 -0400

    submitted algorithim to stack overflow

--- tictactoe/cmd/no-pointer/stack_overflow/game.cpp
+++ tictactoe/cmd/no-pointer/stack_overflow/game.cpp
@@ -77,22 +77,28 @@ int validate(int (rep)[3][3]){
 
 
 
-//recursive function that does the hair splitting
+//recursive function that splits past Stage 1
+//end goal of function: reduce a specific scenario of board to a stage number outlining how many moves required until victory
+//or -1, where loss is inevitable
 int bestWin(int (rep)[3][3], int stage){
 	cout << "i got here too" << endl;
 	if(validate(rep)==2){return -1;}
 	else if(validate(rep) == 1 or validate(rep) == 3){return stage;}
 	else{
-		cout << "branching type board" << endl;
-		int arrLength = 0; //holds arrlength for eventual stage numbers
+		//creates array to hold subsequent stage values
+		//length is determined by getting amount of remaining free spaces (0)
+		int arrLength = 0;
 		for(int i = 0; i < 3; i++){
 			for(int a = 0; a <3; a++){
 				if((rep)[i][a]==0){arrLength++;}
 			}	
 		}
-		cout << "length of" << stage << "stage array: " << arrLength << endl;
 		int stages [arrLength];
 		int stageCount=0;
+
+		//ieterates through the function looking for 0s
+		//from there creates a new array where cpu/player 'input' inserted into detected free space
+		//runs bestWin again for that function, stage number stored into stages
 		for(int i = 0; i < 3; i++){
 			for(int a = 0; a < 3; a++){
 				cout << "aaaa" << endl;
@@ -111,14 +117,13 @@ int bestWin(int (rep)[3][3], int stage){
 					*/
 					if(stage%2==0){duplicate[i][a]=2;} //this may have to change
 					else{duplicate[i][a]=1;}
-					cout << "here is the array for stage " << stage << endl;
-					printBoard(duplicate);
 					stages[stageCount] = bestWin(duplicate,stage++);
 					stageCount++;	
 				}	
 			}	
 		}
-		//calculates least smount of stages for a win
+		//determines the smallest stage number (i.e. least possible number of steps to victory)
+		//PROBLEM 1: if stages is printed, its length grows
 		int output = sizeof(stages);
 		for(int i = 0; i < sizeof(stages); i++){
 			if(stages[i] < output and stages[i] > 0){output = stages[i];}	
@@ -129,67 +134,59 @@ int bestWin(int (rep)[3][3], int stage){
 
 
 
-//"master" version of bestWin: holds stage 1 array and from there determines the best case to go for
+//master version of bestWin: exactly it except the array is preserved
+//reason: the output for this function will be used for the actual computer move
 int cpuCalc(int (rep)[3][3]){
-//need some advice	
-/*
-algorithim structure:
-
-1) create array of possible moves, which ones lead to failure, and how many moves out of it
-2) create recursive function that goes through each of the scenarios (they will all branch out)
-3) determine from there a situation that will lead to a win scenario (or a tie scenario) in the least number of moves
-*/
-	cout << "I got here" << endl;
+	//checks if game has just started
 	if(isEmpty(rep)){
 		input(rep,1,1,1);
 		return 0;
 	}
 	else
 	{
+
+	//computes array of possible scenarios to hold stage values in
 	int arrLen = 0;
 	for(int i =0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
 			if((rep)[i][a]==0){arrLen++;}	
 		}	
 	}
-	cout << "arrLen" << arrLen << endl;
 	int stages [arrLen];
 	int stageCount = 0;
+
+
+	//ietrates through game board, searching for empty spaces
+	//plugs in computer value (this function determines actual computer move) into hypothetical array
+	//and runs bestWin on it
+	//output value goes into array of possible stages
 	for(int i =0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
 			if((rep)[i][a]==0){
+				//generates temp array which holds possible game scenario
 				int dupli[3][3];
 				for(int x = 0; x < 3; x++){
 					for(int y = 0; y < 3; y++){
 						dupli[x][y]=rep[x][y];	
 					}	
 				}
-				/*
-				for(int d =0; d < 9; d++){
-					std::copy((rep)+d, dupli);//change here	
-				}
-				*/
 				dupli[i][a]=1;
-				cout << "here is a case" << endl;
-				printBoard(dupli);
 				stages[stageCount] = bestWin(dupli,2);	
-				cout << "here is the stage number for the case: " << stages[stageCount] << endl;
 				stageCount++;
 			}	
 		}	
 	}
-	cout << "The size of the array is now" << sizeof(stages) << "and here is the array\n[";
-	for(int i = 0; i < sizeof(stages); i++){
-		cout << stages[i] << ",";	
-	}	
-	cout << "]" << endl;
+	//determines the smallest stage value that's not -1 and records its position
+	//a second for loop will determine the position number and input value into board
 	int eval = arrLen, movePlace=-5;
-	//determines the smallest stage 1 scenario, and its position
 	for(int i = 0; i < arrLen; i++){
 		if(stages[i] < eval and stages[i] > 0){eval=stages[i]; movePlace = i;}
 	}
+
+
+	//this function finds the proper 'free space' using the previously gotten value
+	//puts in the computer value and plugs it into computer board
 	int testMove = 0;
-	//from there re-loops through the array, finds the proper zero and maps a 1 there
 	for(int i = 0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
 			if(testMove==movePlace){input((rep),i,a,1);}	
