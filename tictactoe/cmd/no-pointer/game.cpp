cac80dc433998b97fc629f9885a75da5329decea
Brian Lee <blee025432@gmail.com>
2017-06-16 08:55:27 -0400

    help

--- tictactoe/cmd/no-pointer/game.cpp
+++ tictactoe/cmd/no-pointer/game.cpp
@@ -5,18 +5,41 @@
 using namespace std;
 
 
-int input(int (*rep)[3][3], int i, int a, int input){
-	(*rep)[i][a]=input;
+int printBoard(int (rep)[3][3]){
+	for(int i = 0; i < 3; i++){
+		for(int a = 0; a < 3; a++){
+			cout << (rep)[i][a] << " ";	
+		}
+		cout << endl;
+	}
 	return 0;
 }
 
 
 
-bool isFull(int (*rep)[3][3]){
+int input(int (rep)[3][3], int i, int a, int input){
+	(rep)[i][a]=input;
+	return 0;
+}
+
+//dtermines if game board is EMPTY (opener minimax function)
+bool isEmpty(int rep[3][3]){
+	bool test = true;
+	for(int i = 0; i < 3; i++){
+		for(int a = 0; a < 3; a++){
+			if(rep[i][a]!=0){test = false;}	
+		}	
+	}
+	return test;	
+}
+
+
+//determines if board is FULL (for validaton purposes)
+bool isFull(int (rep)[3][3]){
 	bool test = true;
 	for(int i = 0; i < 3; i++){
 		for(int a=0; a < 3; a++){
-			if((*rep)[i][a] == 0){test = false;}	
+			if((rep)[i][a] == 0){test = false;}	
 		}	
 	}	
 	return test;
@@ -26,26 +49,26 @@ bool isFull(int (*rep)[3][3]){
 
 //0 for no winner, 1 for cpu, 2 for user. requires later testing for ties
 //needs bugtesting
-int validate(int (*rep)[3][3]){
+int validate(int (rep)[3][3]){
 	int output = 0;
-	if((*rep)[0][0]== 1 && (*rep)[0][1]== 1 && (*rep)[0][2]==1){cout<<"a";output = 1;}
-	if((*rep)[0][0]== 2 && (*rep)[0][1]== 2 && (*rep)[0][2]==2){cout<<"b";output = 2;}
-	if((*rep)[1][0]== 1 && (*rep)[1][1]== 1 && (*rep)[1][2]==1){cout<<"c";output = 1;}
-	if((*rep)[1][0]== 2 && (*rep)[1][1]== 2 && (*rep)[1][2]==2){cout<<"d";output = 2;}
-	if((*rep)[2][0]== 1 && (*rep)[2][1]== 1 && (*rep)[2][2]==1){cout<<"e";output = 1;}
-	if((*rep)[2][0]== 2 && (*rep)[2][1]== 2 && (*rep)[2][2]==2){cout<<"f";output = 2;}
+	if((rep)[0][0]== 1 && (rep)[0][1]== 1 && (rep)[0][2]==1){cout<<"a";output = 1;}
+	if((rep)[0][0]== 2 && (rep)[0][1]== 2 && (rep)[0][2]==2){cout<<"b";output = 2;}
+	if((rep)[1][0]== 1 && (rep)[1][1]== 1 && (rep)[1][2]==1){cout<<"c";output = 1;}
+	if((rep)[1][0]== 2 && (rep)[1][1]== 2 && (rep)[1][2]==2){cout<<"d";output = 2;}
+	if((rep)[2][0]== 1 && (rep)[2][1]== 1 && (rep)[2][2]==1){cout<<"e";output = 1;}
+	if((rep)[2][0]== 2 && (rep)[2][1]== 2 && (rep)[2][2]==2){cout<<"f";output = 2;}
 
-	if((*rep)[0][0]== 1 && (*rep)[1][0]== 1 && (*rep)[2][0]==1){cout<<"g";output = 1;}
-	if((*rep)[0][0]== 2 && (*rep)[1][0]== 2 && (*rep)[2][0]==2){cout<<"h";output = 2;}
-	if((*rep)[0][1]== 1 && (*rep)[1][1]== 1 && (*rep)[2][1]==1){cout<<"i";output = 1;}
-	if((*rep)[0][1]== 2 && (*rep)[1][1]== 2 && (*rep)[2][1]==2){cout<<"j";output = 2;}
-	if((*rep)[0][2]== 1 && (*rep)[1][2]== 1 && (*rep)[2][2]==1){cout<<"k";output = 1;}
-	if((*rep)[0][2]== 2 && (*rep)[1][2]== 2 && (*rep)[2][2]==2){cout<<"l";output = 2;}
+	if((rep)[0][0]== 1 && (rep)[1][0]== 1 && (rep)[2][0]==1){cout<<"g";output = 1;}
+	if((rep)[0][0]== 2 && (rep)[1][0]== 2 && (rep)[2][0]==2){cout<<"h";output = 2;}
+	if((rep)[0][1]== 1 && (rep)[1][1]== 1 && (rep)[2][1]==1){cout<<"i";output = 1;}
+	if((rep)[0][1]== 2 && (rep)[1][1]== 2 && (rep)[2][1]==2){cout<<"j";output = 2;}
+	if((rep)[0][2]== 1 && (rep)[1][2]== 1 && (rep)[2][2]==1){cout<<"k";output = 1;}
+	if((rep)[0][2]== 2 && (rep)[1][2]== 2 && (rep)[2][2]==2){cout<<"l";output = 2;}
 
-	if((*rep)[0][0]== 1 && (*rep)[1][1]== 1 && (*rep)[2][2]==1){cout<<"m";output=1;}
-	if((*rep)[0][0]== 2 && (*rep)[1][1]== 2 && (*rep)[2][2]==2){cout<<"n";output=2;}
-	if((*rep)[0][2]== 1 && (*rep)[1][1]== 1 && (*rep)[2][0]==1){cout<<"o";output=1;}
-	if((*rep)[0][2]== 2 && (*rep)[1][1]== 2 && (*rep)[2][0]==2){cout<<"p";output=2;}
+	if((rep)[0][0]== 1 && (rep)[1][1]== 1 && (rep)[2][2]==1){cout<<"m";output=1;}
+	if((rep)[0][0]== 2 && (rep)[1][1]== 2 && (rep)[2][2]==2){cout<<"n";output=2;}
+	if((rep)[0][2]== 1 && (rep)[1][1]== 1 && (rep)[2][0]==1){cout<<"o";output=1;}
+	if((rep)[0][2]== 2 && (rep)[1][1]== 2 && (rep)[2][0]==2){cout<<"p";output=2;}
 
 	if (isFull(rep) == true and output == 0){output = 3;}
 
@@ -55,31 +78,31 @@ int validate(int (*rep)[3][3]){
 
 
 //recursive function that does the hair splitting
-int bestWin(int (*rep)[3][3], int stage){
+int bestWin(int (rep)[3][3], int stage){
 	if(validate(rep)==2){return -1;}
 	else if(validate(rep) == 1 or validate(rep) == 3){return stage;}
 	else{
 		int arrLength = 0; //holds arrlength for eventual stage numbers
 		for(int i = 0; i < 3; i++){
 			for(int a = 0; a <3; a++){
-				if((*rep)[i][a]==0){arrLength++;}
+				if((rep)[i][a]==0){arrLength++;}
 			}	
 		}
 		int stages [arrLength];
 		int stageCount=0;
 		for(int i = 0; i < 3; i++){
 			for(int a = 0; a < 3; a++){
-				if((*rep)[i][a]=0){
+				if((rep)[i][a]=0){
 					int duplicate[3][3] = {0,0,0,0,0,0,0,0,0};
 					//copies the values of rep over to duplicate, value by value
 					for(int x = 0; x < 3; x++){
 						for(int y = 0; y < 3; y++){
-							duplicate[x][y]=*rep[x][y];	
+							duplicate[x][y]=rep[x][y];	
 						}	
 					}
 					/*
 					for(int c = 0; i < 9; i++){
-						std::copy((*rep)+c,duplicate);
+						std::copy((rep)+c,duplicate);
 					}
 					*/
 					if(stage%2==0){duplicate[i][a]=2;} //this may have to change
@@ -102,7 +125,7 @@ int bestWin(int (*rep)[3][3], int stage){
 
 
 //"master" version of bestWin: holds stage 1 array and from there determines the best case to go for
-int cpuCalc(int (*rep)[3][3]){
+int cpuCalc(int (rep)[3][3]){
 //need some advice	
 /*
 algorithim structure:
@@ -111,33 +134,49 @@ algorithim structure:
 2) create recursive function that goes through each of the scenarios (they will all branch out)
 3) determine from there a situation that will lead to a win scenario (or a tie scenario) in the least number of moves
 */
+	cout << "I got here" << endl;
+	if(isEmpty(rep)){
+		input(rep,1,1,1);
+		return 0;
+	}
+	else
+	{
 	int arrLen = 0;
 	for(int i =0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
-			if((*rep)[i][a]==0){arrLen++;}	
+			if((rep)[i][a]==0){arrLen++;}	
 		}	
 	}
+	cout << "arrLen" << arrLen << endl;
 	int stages [arrLen];
 	int stageCount = 0;
 	for(int i =0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
-			if((*rep)[i][a]==0){
+			if((rep)[i][a]==0){
 				int dupli[3][3];
 				for(int x = 0; x < 3; x++){
 					for(int y = 0; y < 3; y++){
-						dupli[x][y]=*rep[x][y];	
+						dupli[x][y]=rep[x][y];	
 					}	
 				}
 				/*
 				for(int d =0; d < 9; d++){
-					std::copy((*rep)+d, dupli);//change here	
+					std::copy((rep)+d, dupli);//change here	
 				}
 				*/
 				dupli[i][a]=1;
+				cout << "here is a case" << endl;
+				printBoard(dupli);
 				stages[stageCount] = bestWin(dupli,2);	
+				stageCount++;
 			}	
 		}	
 	}
+	cout << "The size of the array is now" << sizeof(stages) << "and here is the array\n[";
+	for(int i = 0; i < sizeof(stages); i++){
+		cout << stages[i] << ",";	
+	}	
+	cout << "]" << endl;
 	int eval = arrLen, movePlace=-5;
 	for(int i = 0; i < arrLen; i++){
 		if(stages[i] < eval and stages[i] > 0){eval=stages[i]; movePlace = i;}
@@ -146,59 +185,49 @@ algorithim structure:
 	for(int i = 0; i < 3; i++){
 		for(int a = 0; a < 3; a++){
 			testMove++;
-			if(testMove==movePlace){input((*rep),i,a,1);}	
+			if(testMove==movePlace){input((rep),i,a,1);}	
 		}	
 	}
 	return 0;
-
-}
-
-int printBoard(int (*rep)[3][3]){
-	for(int i = 0; i < 3; i++){
-		for(int a = 0; a < 3; a++){
-			cout << (*rep)[i][a] << " ";	
-		}
-		cout << endl;
 	}
-	return 0;
 }
-
 int main(){
 	int board[3][3] = {
-		{1,2,0},
-		{2,1,0},
-		{1,2,0}	
+		{0,0,0},
+		{0,0,0},
+		{0,0,0}	
 	};
 	bool test = true;
 	while(test){
-		cpuCalc(&board);
+		cpuCalc(board);
 		cout << "enter coords" << endl;
 		int i;
 		cin >> i;
 		int a;
 		cin >> a;
-		input(&board, i, a, 2);
-		if(validate(&board) == 0){
+		input(board, i, a, 2);
+		if(validate(board) == 0){
 			cout << "The game continues, here is the board" << endl;
-			printBoard(&board);	
+			printBoard(board);	
 		}
-		else if(validate(&board)==1){
+		else if(validate(board)==1){
 			cout << "It's a win for the computer" << endl;
 			test = false;	
-			printBoard(&board);
+			printBoard(board);
 		}
-		else if(validate(&board) == 2){
+		else if(validate(board) == 2){
 			cout << "It's a win for the player" << endl;	
 			test = false;
 		}
-		else if(validate(&board) == 3){
+		else if(validate(board) == 3){
 			cout << "it's a tie" << endl;
-			printBoard(&board);
+			printBoard(board);
 			test=false;
 		}
 	}
-	//int (*rep)[3][3] = &board;
+	//int (rep)[3][3] = &board;
 	return 0;
 
 
 }
+
