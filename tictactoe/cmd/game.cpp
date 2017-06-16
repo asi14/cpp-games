4337639f41a6c0b0f9079603a46ef739e4b8a79b
Brian Lee <blee025432@gmail.com>
2017-06-15 23:02:33 -0400

    reordrered methods

--- tictactoe/cmd/game.cpp
+++ tictactoe/cmd/game.cpp
@@ -4,6 +4,95 @@
 
 using namespace std;
 
+
+int input(int (*rep)[3][3], int i, int a, int input){
+	(*rep)[i][a]=input;
+	return 0;
+}
+
+
+
+bool isFull(int (*rep)[3][3]){
+	bool test = true;
+	for(int i = 0; i < 3; i++){
+		for(int a=0; a < 3; a++){
+			if((*rep)[i][a] == 0){test = false;}	
+		}	
+	}	
+	return test;
+}
+
+
+
+//0 for no winner, 1 for cpu, 2 for user. requires later testing for ties
+//needs bugtesting
+int validate(int (*rep)[3][3]){
+	int output = 0;
+	if((*rep)[0][0]== 1 && (*rep)[0][1]== 1 && (*rep)[0][2]==1){cout<<"a";output = 1;}
+	if((*rep)[0][0]== 2 && (*rep)[0][1]== 2 && (*rep)[0][2]==2){cout<<"b";output = 2;}
+	if((*rep)[1][0]== 1 && (*rep)[1][1]== 1 && (*rep)[1][2]==1){cout<<"c";output = 1;}
+	if((*rep)[1][0]== 2 && (*rep)[1][1]== 2 && (*rep)[1][2]==2){cout<<"d";output = 2;}
+	if((*rep)[2][0]== 1 && (*rep)[2][1]== 1 && (*rep)[2][2]==1){cout<<"e";output = 1;}
+	if((*rep)[2][0]== 2 && (*rep)[2][1]== 2 && (*rep)[2][2]==2){cout<<"f";output = 2;}
+
+	if((*rep)[0][0]== 1 && (*rep)[1][0]== 1 && (*rep)[2][0]==1){cout<<"g";output = 1;}
+	if((*rep)[0][0]== 2 && (*rep)[1][0]== 2 && (*rep)[2][0]==2){cout<<"h";output = 2;}
+	if((*rep)[0][1]== 1 && (*rep)[1][1]== 1 && (*rep)[2][1]==1){cout<<"i";output = 1;}
+	if((*rep)[0][1]== 2 && (*rep)[1][1]== 2 && (*rep)[2][1]==2){cout<<"j";output = 2;}
+	if((*rep)[0][2]== 1 && (*rep)[1][2]== 1 && (*rep)[2][2]==1){cout<<"k";output = 1;}
+	if((*rep)[0][2]== 2 && (*rep)[1][2]== 2 && (*rep)[2][2]==2){cout<<"l";output = 2;}
+
+	if((*rep)[0][0]== 1 && (*rep)[1][1]== 1 && (*rep)[2][2]==1){cout<<"m";output=1;}
+	if((*rep)[0][0]== 2 && (*rep)[1][1]== 2 && (*rep)[2][2]==2){cout<<"n";output=2;}
+	if((*rep)[0][2]== 1 && (*rep)[1][1]== 1 && (*rep)[2][0]==1){cout<<"o";output=1;}
+	if((*rep)[0][2]== 2 && (*rep)[1][1]== 2 && (*rep)[2][0]==2){cout<<"p";output=2;}
+
+	if (isFull(rep) == true and output == 0){output = 3;}
+
+	return output;
+}
+
+
+
+//recursive function that does the hair splitting
+int bestWin(int (*rep)[3][3], int stage){
+	if(validate(rep)==2){return -1;}
+	else if(validate(rep) == 1 or validate(rep) == 3){return stage;}
+	else{
+		int arrLength = 0; //holds arrlength for eventual stage numbers
+		for(int i = 0; i < 3; i++){
+			for(int a = 0; a <3; a++){
+				if((*rep)[i][a]==0){arrLength++;}
+			}	
+		}
+		int stages [arrLength];
+		int stageCount=0;
+		for(int i = 0; i < 3; i++){
+			for(int a = 0; a < 3; a++){
+				if((*rep)[i][a]=0){
+					int duplicate[3][3];
+					for(int c = 0; i < 9; i++){
+						std::copy((*rep)+c,duplicate);
+					}
+					if(stage%2==0){duplicate[i][a]=2;} //this may have to change
+					else{duplicate[i][a]=1;}
+
+					stages[stageCount] = bestWin(duplicate, stage++);
+					stageCount++;	
+				}	
+			}	
+		}
+		//calculates least smount of stages for a win
+		int output = sizeof(stages);
+		for(int i = 0; i < sizeof(stages); i++){
+			if(stages[i] < output){output = stages[i];}	
+		}
+		return output;	
+	}
+}
+
+
+
 //"master" version of bestWin: holds stage 1 array and from there determines the best case to go for
 int cpuCalc(int (*rep)[3][3]){
 //need some advice	
@@ -47,85 +136,6 @@ algorithim structure:
 	}
 	return 0;
 
-}
-
-//recursive function that does the hair splitting
-int bestWin(int (*rep)[3][3], int stage){
-	if(validate(rep)==2){return -1;}
-	else if(validate(rep) == 1 or validate(rep) == 3){return stage;}
-	else{
-		int arrLength = 0; //holds arrlength for eventual stage numbers
-		for(int i = 0; i < 3; i++){
-			for(int a = 0; a <3; a++){
-				if((*rep)[i][a]==0){arrLength++;}
-			}	
-		}
-		int stages [arrLength];
-		int stageCount=0;
-		for(int i = 0; i < 3; i++){
-			for(int a = 0; a < 3; a++){
-				if((*rep)[i][a]=0){
-					int duplicate[3][3];
-					for(int c = 0; i < 9; i++){
-						std::copy((*rep)+c,duplicate);
-					}
-					if(stage%2==0){duplicate[i][a]=2;} //this may have to change
-					else{duplicate[i][a]=1;}
-
-					stages[stageCount] = bestWin(duplicate, stage++);
-					stageCount++;	
-				}	
-			}	
-		}
-		//calculates least smount of stages for a win
-		int output = sizeof(stages);
-		for(int i = 0; i < sizeof(stages); i++){
-			if(stages[i] < output){output = stages[i];}	
-		}
-		return output;	
-	}
-}
-int input(int (*rep)[3][3], int i, int a, int input){
-	(*rep)[i][a]=input;
-	return 0;
-}
-
-bool isFull(int (*rep)[3][3]){
-	bool test = true;
-	for(int i = 0; i < 3; i++){
-		for(int a=0; a < 3; a++){
-			if((*rep)[i][a] == 0){test = false;}	
-		}	
-	}	
-	return test;
-}
-
-//0 for no winner, 1 for cpu, 2 for user. requires later testing for ties
-//needs bugtesting
-int validate(int (*rep)[3][3]){
-	int output = 0;
-	if((*rep)[0][0]== 1 && (*rep)[0][1]== 1 && (*rep)[0][2]==1){cout<<"a";output = 1;}
-	if((*rep)[0][0]== 2 && (*rep)[0][1]== 2 && (*rep)[0][2]==2){cout<<"b";output = 2;}
-	if((*rep)[1][0]== 1 && (*rep)[1][1]== 1 && (*rep)[1][2]==1){cout<<"c";output = 1;}
-	if((*rep)[1][0]== 2 && (*rep)[1][1]== 2 && (*rep)[1][2]==2){cout<<"d";output = 2;}
-	if((*rep)[2][0]== 1 && (*rep)[2][1]== 1 && (*rep)[2][2]==1){cout<<"e";output = 1;}
-	if((*rep)[2][0]== 2 && (*rep)[2][1]== 2 && (*rep)[2][2]==2){cout<<"f";output = 2;}
-
-	if((*rep)[0][0]== 1 && (*rep)[1][0]== 1 && (*rep)[2][0]==1){cout<<"g";output = 1;}
-	if((*rep)[0][0]== 2 && (*rep)[1][0]== 2 && (*rep)[2][0]==2){cout<<"h";output = 2;}
-	if((*rep)[0][1]== 1 && (*rep)[1][1]== 1 && (*rep)[2][1]==1){cout<<"i";output = 1;}
-	if((*rep)[0][1]== 2 && (*rep)[1][1]== 2 && (*rep)[2][1]==2){cout<<"j";output = 2;}
-	if((*rep)[0][2]== 1 && (*rep)[1][2]== 1 && (*rep)[2][2]==1){cout<<"k";output = 1;}
-	if((*rep)[0][2]== 2 && (*rep)[1][2]== 2 && (*rep)[2][2]==2){cout<<"l";output = 2;}
-
-	if((*rep)[0][0]== 1 && (*rep)[1][1]== 1 && (*rep)[2][2]==1){cout<<"m";output=1;}
-	if((*rep)[0][0]== 2 && (*rep)[1][1]== 2 && (*rep)[2][2]==2){cout<<"n";output=2;}
-	if((*rep)[0][2]== 1 && (*rep)[1][1]== 1 && (*rep)[2][0]==1){cout<<"o";output=1;}
-	if((*rep)[0][2]== 2 && (*rep)[1][1]== 2 && (*rep)[2][0]==2){cout<<"p";output=2;}
-
-	if (isFull(rep) == true and output == 0){output = 3;}
-
-	return output;
 }
 
 int printBoard(int (*rep)[3][3]){
